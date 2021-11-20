7564b811be5f0f58736bd208d3665bea2d7c0fa9
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2021-11-20 22:15:10 +0100

    Feat.added fetch all and fetch one todo-list item

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
 
+use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('todo-list', [TodoListController::class, 'index'])
+    ->name('todo-list.index');
+Route::get('todo-list/{id}', [TodoListController::class, 'show'])
+    ->name('todo-list.show');
