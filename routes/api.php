a24a6043c4ee91ccdcd861677ab8b7df7afaa2ad
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2021-11-21 20:59:05 +0100

    Fix.refactored controller and test. controller now makes use of Route Model Binding for returning single todo-list.

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('todo-list', [TodoListController::class, 'index'])
     ->name('todo-list.index');
-Route::get('todo-list/{id}', [TodoListController::class, 'show'])
+Route::get('todo-list/{todoList}', [TodoListController::class, 'show'])
     ->name('todo-list.show');
